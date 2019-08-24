69cbf418b2eb47344fb24a5fff49bfd60d91f55a
idanhakim <idanhakim123@gmail.com>
2019-08-24 21:59:52 +0300

    Updates

---
+++ precache-manifest.ba82e96b23dceb8a4c6e31730a45f2d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "941949a29e0e00a37f583db549573b35",
+    "url": "/test1/index.html"
+  },
+  {
+    "revision": "9b9c5836dce6025b9cf9",
+    "url": "/test1/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "223951d87b5cdb778119",
+    "url": "/test1/static/js/2.58af1d4d.chunk.js"
+  },
+  {
+    "revision": "9b9c5836dce6025b9cf9",
+    "url": "/test1/static/js/main.c244351d.chunk.js"
+  },
+  {
+    "revision": "9bdcebc00d60f5021adb",
+    "url": "/test1/static/js/runtime~main.b37276f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test1/static/media/logo.5d5d9eef.svg"
+  }
+]);
