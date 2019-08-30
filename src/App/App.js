801188b13320cbcd9c4dfd8fc89b45f0238048b0
idanhakim <idanhakim123@gmail.com>
2019-08-30 15:49:18 +0300

    update

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from "react-router-dom";
-import './App.css';
-import Header from "../Components/Header";
-import AboutUs from "../Pages/AboutUs";
-import ContactUs from "../Pages/ContactUs";
-import Home from "../Pages/Home";
-import Footer from "../Components/Footer";
+import {BrowserRouter as Router} from "react-router-dom";
+import RoutesApp from '../Pages/routes'
+import '../Styles/reset.scss';
 
 const App = () => {
-    const preUrl = `${process.env.NODE_ENV === 'development' ? '/' : '/Netmapp/'}`;
+
     return (
         <Router>
-            <Route path={preUrl} component={Header}/>
-            <Route path={preUrl} exact component={Home}/>
-            <Route path={`${preUrl}about-us/`} component={AboutUs}/>
-            <Route path={`${preUrl}contact-us/`} component={ContactUs}/>
-            <Route path={preUrl} component={Footer}/>
+            <RoutesApp />
         </Router>
     );
 };
--- src/Pages/routes.js
+++ src/Pages/routes.js
@@ -1,10 +1,47 @@
+import React from "react";
+import {Route} from "react-router-dom";
+
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import Home from "./Home";
+import AboutUs from "./AboutUs";
+import ContactUs from "./ContactUs";
+
+const preUrl = `${process.env.NODE_ENV === 'development' ? '/' : '/Netmapp/'}`;
+
 const routes = [
     {
-       url:'/',
-       component: '',
-       isExact: false,
-       label: ''
+        path: preUrl,
+        component: Header,
+        exact: false,
+        label: ''
+    },
+    {
+        path: preUrl,
+        component: Home,
+        exact: true,
+        label: ''
+    },
+    {
+        path: `${preUrl}about-us/`,
+        component: AboutUs,
+        exact: true,
+        label: ''
+    },
+    {
+        path: `${preUrl}contact-us/`,
+        component: ContactUs,
+        exact: true,
+        label: ''
+    },
+    {
+        path: preUrl,
+        component: Footer,
+        exact: false,
+        label: ''
     }
 ];
 
-export default routes;
+const RoutesApp = () => routes.map((route, i) => <Route key={i} {...route} />);
+
+export default RoutesApp;
