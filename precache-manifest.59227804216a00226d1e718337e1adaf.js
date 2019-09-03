50b7433021d3307d92666be113b4bca67368a048
idanhakim <idanhakim123@gmail.com>
2019-09-04 02:18:23 +0300

    Updates

---
+++ precache-manifest.59227804216a00226d1e718337e1adaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6cc9ff7c166c572537d5d1d89145ab",
+    "url": "/Netmapp/index.html"
+  },
+  {
+    "revision": "d073717d3e63532beac8",
+    "url": "/Netmapp/static/css/main.b330df5c.chunk.css"
+  },
+  {
+    "revision": "f68151a7ecbe9a8e382f",
+    "url": "/Netmapp/static/js/2.22e769ca.chunk.js"
+  },
+  {
+    "revision": "d073717d3e63532beac8",
+    "url": "/Netmapp/static/js/main.8249f4f5.chunk.js"
+  },
+  {
+    "revision": "22153e59cd80d60e0675",
+    "url": "/Netmapp/static/js/runtime~main.e1ff80ce.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Netmapp/precache-manifest.bb279e16df20874fdd13615704b87e53.js"
+  "/Netmapp/precache-manifest.59227804216a00226d1e718337e1adaf.js"
 );
 
 self.addEventListener('message', (event) => {
